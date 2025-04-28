3f60f98589def44d0f5f44aa09ca1fbf1eed501f
Basani somesh <150121746+Someshbasani@users.noreply.github.com>
2025-04-28 15:20:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load the trained model
-model = joblib.load("best_XGBoost_model.pkl")
+model = joblib.load("best_random_forest_model.pkl")
 
 st.set_page_config(page_title="Disease Prediction", layout="centered")
 st.title("Disease Prediction using XDBoost")
